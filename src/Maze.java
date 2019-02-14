5269014ce0c1845eaeab468e8c8385c1cc83140f
ajafari <ajafari@ajafaris-MacBook-Pro.local>
2019-02-14 15:33:45 -0500

    init commit

--- src/Maze.java
+++ src/Maze.java
@@ -1,28 +1,17 @@
-import javalib.colors.Black;
-import javalib.colors.White;
+
 import javalib.impworld.World;
-import javalib.worldimages.Posn;
-import javalib.worldimages.RectangleImage;
-import javalib.worldimages.TriangleImage;
-import javalib.worldimages.WorldImage;
+import javalib.impworld.WorldScene;
+import javalib.worldimages.*;
 
 import java.awt.*;
 
 public class Maze extends World {
-    @Override
-    public WorldImage makeImage() {
-        int width = 10;
-        int PIXEL = 30;
-        int height = 20;
-        WorldImage board =
-                new RectangleImage(new Posn(width * PIXEL / 2,
-                        height * PIXEL / 2),
-                        width * PIXEL, height * PIXEL, new White());
-        Posn a = new Posn(10,20);
-        Posn b = new Posn(10,50);
-        Posn c = new Posn(0,20);
-        WorldImage triangle = new TriangleImage(a, b, c, Color.BLACK);
-        return board.overlayImages(triangle);
 
+    @Override
+    public WorldScene makeScene() {
+        WorldScene board = new WorldScene(500,500);
+        WorldImage circle = new CircleImage(10, OutlineMode.SOLID, Color.RED);
+        board.placeImageXY(circle, 20, 10);
+        return board;
     }
 }
